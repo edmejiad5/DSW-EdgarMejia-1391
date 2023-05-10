a3fba34d0926a1f1893d1dd9e519e15917be8e69
EdgarMejia <dariom.901023@gmail.com>
2023-05-10 00:27:16 -0500

    Fase4
    Desarrollo de la Fase 4 actividad individual y colaborativa.

---
+++ fase4/js/script1.js
@@ -0,0 +1,22 @@
+function procesarRespuestas1(){
+    let total = 5
+    let puntos = 0;
+
+    let myForm =document.forms["formulario1"];
+    let respuestasCorrectas =["a", "c", "c", "b", "d",]
+    
+
+    for (let i=1; i<= total; i++){
+        if (myForm["r" + i].value == null ||
+            myForm["r" + i].value == '') {
+            alert('Favor responder todas las preguntas del cuestionario');
+            return false;    
+        } else {
+            if (myForm["r" + i].value === respuestasCorrectas[i -1])
+                puntos++;
+        }
+    }
+    let resultado = document.getElementById('resultado1')
+    resultado.innerHTML='Obtuviste '+puntos+' puntos de ' + total + ' posibles ';
+    return false;
+}
